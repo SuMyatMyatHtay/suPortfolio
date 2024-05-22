2a4727d7e11e3fe89d5c929196dbabb2dfe2c997
SuMyatMyatHtay <summh11@gmail.com>
2024-05-22 22:37:55 +0800

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './content/home/home.component';
+import { AboutmeComponent } from './content/aboutme/aboutme.component';
+import { ProjectsComponent } from './content/projects/projects.component';
+import { ContentComponent } from './content/content.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'content/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'content',
+    component: ContentComponent,
+  },
+  {
+    path: 'content/home',
+    component: HomeComponent,
+  },
+  {
+    path: 'content/aboutme',
+    component: AboutmeComponent,
+  },
+  {
+    path: 'content/projects',
+    component: ProjectsComponent,
+  },
+];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { DarkModeService } from '../services/dark-mode.service';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterModule, RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   darkModeService: DarkModeService = inject(DarkModeService);
+  isMenuOpen = false;
 
-  toggleDarkMode(){
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  toggleDarkMode() {
     this.darkModeService.updateDarkMode();
   }
 }
